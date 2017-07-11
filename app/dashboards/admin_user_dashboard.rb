ef19a41c385eb49df6c7cc63d837c1a9f36e34dd
Ben Critchfield <ben.critchfield8@gmail.com>
2017-07-11 16:10:12 -0600

    added admin dashboard permissions

--- app/dashboards/admin_user_dashboard.rb
+++ app/dashboards/admin_user_dashboard.rb
@@ -27,7 +27,7 @@ class AdminUserDashboard < Administrate::BaseDashboard
   SHOW_PAGE_ATTRIBUTES = [
     :posts,
     :id,
-    :email,
+    :email,d
     :sign_in_count,
     :current_sign_in_at,
     :last_sign_in_at,
